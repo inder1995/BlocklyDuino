cc65f046884a5ad60d117ea2c1be7fe0f5e3c10e
inder1995 <singhinderpreet1995@gamil.com>
2017-07-29 04:49:49 -0700

    Update khelo_potentiometer.js

--- blockly/generators/arduino/khelo_potentiometer.js
+++ blockly/generators/arduino/khelo_potentiometer.js
@@ -3,8 +3,16 @@ goog.provide('Blockly.Arduino.khelo_potentiometer');
 goog.require('Blockly.Arduino');
 
 
-Blockly.Arduino.base_delay_example = function() {
-  var delay_time = Blockly.Arduino.valueToCode(this, 'DELAY_TIME', Blockly.Arduino.ORDER_ATOMIC) || '1000'
-  var code = 'delay(' + delay_time + ');\n';
-  return code;
+Blockly.Arduino['khelo_potentiometer'] = function(block) {
+  var dropdown_pin_options = block.getFieldValue('pin_options');
+  // TODO: Assemble Arduino into code variable.
+  var code = 'void setup() {  
+pinMode (dropdown_pin_options,INPUT);
+}
+
+void loop() {
+analogRead(dropdown_pin_options);
+}';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Arduino.khelo_potentiometer];
 };
