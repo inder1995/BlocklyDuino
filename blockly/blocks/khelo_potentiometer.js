202abdb84d238a7a90dd110b22ac38a57eaee57c
inder1995 <singhinderpreet1995@gamil.com>
2017-07-29 04:09:38 -0700

    Create khelo_potentiometer.js

---
+++ blockly/blocks/khelo_potentiometer.js
@@ -0,0 +1,7 @@
+Blockly.JavaScript['potentiometer'] = function(block) {
+  var dropdown_pin_options = block.getFieldValue('pin_options');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
