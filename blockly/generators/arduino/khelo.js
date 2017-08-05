c2834310eba07ee4853c9601090d39d6379c5ce5
inder1995 <singhinderpreet1995@gamil.com>
2017-08-04 21:36:54 -0700

    Update khelo.js

--- blockly/generators/arduino/khelo.js
+++ blockly/generators/arduino/khelo.js
@@ -6,11 +6,7 @@ goog.require('Blockly.Arduino');
 Blockly.Arduino['khelo_potentiometer'] = function(block) {
   var dropdown_pin_options = block.getFieldValue('pin_options');
   // TODO: Assemble Arduino into code variable.
-  var code = 'void setup() {  
-pinMode ('+dropdown_pin_options+',INPUT);
-}
-void loop() {
-analogRead('+dropdown_pin_options+');}';
+  var code = 'void setup() { \n pinMode ('+dropdown_pin_options+',INPUT); \n }\n void loop() { \n analogRead('+dropdown_pin_options+');\n}';
   // TODO: Change ORDER_NONE to the correct strength.
   return (code);
 };
