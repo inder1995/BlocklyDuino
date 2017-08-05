f131be325ba75fe78fcb1e0e0d81c29a9bcfb13c
inder1995 <singhinderpreet1995@gamil.com>
2017-08-04 21:34:21 -0700

    Update khelo.js

--- blockly/blocks/khelo.js
+++ blockly/blocks/khelo.js
@@ -4,3 +4,15 @@ goog.provide('Blockly.Blocks.khelo');
 goog.require('Blockly.Blocks');
 
 
+
+Blockly.Blocks["khelo_potentiometer"] = {
+  init: function() {
+    this.setColour(290);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+    this.appendDummyInput()
+        .appendField("Read dimmer at pin")
+        .appendField(new Blockly.FieldDropdown([["A0", "analog_pin_0"], ["A1", "analog_pin_1"]]), "pin_options");
+    this.setOutput(true, "Number");
+      }
+};
