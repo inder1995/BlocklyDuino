cced9bd83585448f217f26e4e43af2ba920305ae
inder1995 <singhinderpreet1995@gamil.com>
2017-08-11 13:23:51 -0700

    Update khelo.js

--- blockly/generators/arduino/khelo.js
+++ blockly/generators/arduino/khelo.js
@@ -18,3 +18,12 @@ Blockly.Arduino['khelo_proximity'] = function(block) {
   // TODO: Change ORDER_NONE to the correct strength.
   return [code];
 };
+
+Blockly.Arduino['khelo_water_detector'] = function(block) {
+  var dropdown_pin_options = block.getFieldValue('pin_options');
+  // TODO: Assemble Arduino into code variable.
+  var code = 'void setup(){ \n pinMode ('+dropdown_pin_options+',INPUT); \n } \n void loop(){ \n if(analogRead('+dropdown_pin_options+')>50) ; \n}';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code];
+};
+
