b9da0798dbeac73b4eba403c2c8a6048619ceb30
inder1995 <singhinderpreet1995@gamil.com>
2017-08-11 13:48:59 -0700

    Update khelo.js

--- blockly/generators/arduino/khelo.js
+++ blockly/generators/arduino/khelo.js
@@ -27,3 +27,26 @@ Blockly.Arduino['khelo_water_detector'] = function(block) {
   return [code];
 };
 
+Blockly.Arduino['khelo_distance'] = function(block) {
+  var dropdown_pin_options = block.getFieldValue('pin_options');
+  // TODO: Assemble Arduino into code variable.
+  var code ='#define ECHOPIN 4 \n
+#define TRIGPIN 7 \n
+void setup() { \n
+Serial.begin(9600); \n
+pinMode(ECHOPIN,INPUT); \n
+pinMode(TRIGPIN,OUTPUT); \n
+}\n
+void loop() { \n
+ digitalWrite(TRIGPIN,LOW); \n
+ delayMicroseconds(4); \n
+ digitalWrite(TRIGPIN,HIGH); \n
+ delayMicroseconds(40); \n
+ digitalWrite(TRIGPIN,LOW); \n 
+ float distance = pulseIn(ECHOPIN,HIGH); \n
+ distance=distance/58; \n
+ Serial.println(distance); \n
+ delay(40); \n }'
+  return [code];
+};
+
